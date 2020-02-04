fb028538f7f4a4b269ffea668c7adc9368c47f41
Eliott Dumeix <eliott.dumeix@veeasystems.com>
2020-02-04 16:20:54 +0100

    add attribute tests.

--- tests/spec_test.go
+++ tests/spec_test.go
@@ -26,14 +26,54 @@ func TestAskPermission(t *testing.T) {
 }
 
 func TestAddAttribute(t *testing.T) {
-	p := setupTest(t, "./scenarios/ask_permission.json")
+	p := setupTest(t, "./scenarios/add_attribute.json")
 	defer p.Stop()
-
 	client := assertNewClient(t)
 
-	resp, err := client.AddAttribute("name", "HEIMAN")
+	attr, err := client.AddAttribute("name", "HEIMAN")
 	assert.NoError(t, err)
-	assert.Equal(t, resp, true)
+	assert.NotNil(t, attr)
 
 	assertPlayerSuccess(t, p)
 }
+
+func TestSetAttribute(t *testing.T) {
+	p := setupTest(t, "./scenarios/set_attribute.json")
+	defer p.Stop()
+	client := assertNewClient(t)
+
+	attr, err := client.AddAttribute("name", "HEIMAN")
+	assert.NoError(t, err)
+	assert.NotNil(t, attr)
+
+	err = attr.SetValue("hello world")
+	assert.NoError(t, err)
+
+	assertPlayerSuccess(t, p)
+}
+
+func TestGetRemoteAttribute(t *testing.T) {
+	p := setupTest(t, "./scenarios/remote_attribute_get.json")
+	defer p.Stop()
+	client := assertNewClient(t)
+
+	remoteAttr, err := client.GetRemoteAttr("test", "remote", client.GetHostname(), "name")
+	assert.NoError(t, err)
+	assert.NotNil(t, remoteAttr)
+
+	val, err := remoteAttr.ReadValue()
+	assert.NoError(t, err)
+	assert.Equal(t, "HEIMAN", val)
+
+	assertPlayerSuccess(t, p)
+}
+
+
+
+
+
+
+
+
+
+
